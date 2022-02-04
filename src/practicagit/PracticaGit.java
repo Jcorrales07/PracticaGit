1a6eb44cd45d247f576694fe0cdbc800458c276c
Joe Corrales <corralesjoe07@gmail.com>
2022-02-04 08:23:21 -0600

    Cambios en el archivp

--- src/practicagit/PracticaGit.java
+++ src/practicagit/PracticaGit.java
@@ -2,6 +2,8 @@ package practicagit;
 
 public class PracticaGit {
     public static void main(String[] args) {
-
+        System.out.println("Hola Cesar");
+        System.out.println("!123");
+        System.out.println("asdasd");
     }
 }
