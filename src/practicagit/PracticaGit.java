99fa9b87a6084c74e3a06329b8f9fe61280faa0d
César Núñez <cesar23augusto16@gmail.com>
2022-02-04 08:55:05 -0600

    int a

--- src/practicagit/PracticaGit.java
+++ src/practicagit/PracticaGit.java
@@ -2,11 +2,6 @@ package practicagit;
 
 public class PracticaGit {
     public static void main(String[] args) {
-        System.out.println("Hola Cesar");
-        System.out.println("!123");
-        System.out.println("asdasd");
-        System.out.println("adasdasd");
-        System.out.println("eh cambio");
-        System.out.println("cambio 2");
+        int a;
     }
 }
